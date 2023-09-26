ba3d1fb7e5767627a2b7207d57d2e83d90bef25b
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2023-09-26 13:04:26 +0530

    submitting code-Tue Sep 26 2023 13:04:24 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,45 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import { error } from "cypress/types/jquery";
 
 const App = () => {
+  const [state, setState] = useState({
+    loading:false,
+    error:null,
+    data:null,
+  })
+
+  const fetchData = async () => {
+    setState({
+      loading:true,
+      error:null,
+      data:null
+    })
+    try {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      setState({
+        loading:false,
+        error:null,
+        data:data,
+      })
+      
+    } catch (error) {
+      setState({
+        loading:false,
+        error:error,
+        data:null
+      })
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>{error.message}</h1>}
+        {data && <h1>{data}</h1>}
     </div>
   )
 }
